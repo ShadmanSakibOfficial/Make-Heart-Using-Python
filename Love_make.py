ebc53300b204ecf98cc56b4b6ce6aba5078f645f
ShadmanSakibOfficial <babuthemetalkid08@gmail.com>
2022-05-25 21:53:43 -0700

    Me

---
+++ Love_make.py
@@ -0,0 +1,16 @@
+from turtle import *
+color("Blue")
+
+begin_fill()  #line tanar shomoy fill kore.
+
+left(50)
+forward(100)  #line tana start korbe.
+
+circle(40, 180)  #love er circle korlo.
+left(260)
+circle(40, 180)
+forward(100)   #shoja jabe.
+
+end_fill()   #fill kora shesh kore.
+
+done()    #line tana shesh kore boshe thakbe. off hoye jabe na.
